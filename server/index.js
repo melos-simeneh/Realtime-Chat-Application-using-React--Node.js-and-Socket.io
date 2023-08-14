645d6d43c902fcdfde2ae9267e1b3a3e5bc97bba
Melos Simeneh Mulu <85923940+melos-simeneh@users.noreply.github.com>
2023-08-14 13:26:56 -0700

    server: index.js update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const http = require(http);
+const cors = require(cors);
 
-app.use(express.json());
-app.use(cors());
+app.use(cors);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, (req, res) => {
-  console.log(`Server is running on port: ${PORT}`);
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log("Server is running");
 });
